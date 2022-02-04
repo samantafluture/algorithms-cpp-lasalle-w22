7e77d442fe337e3d3742bada1d092888ab4fdb45
Samanta Fluture <samantafluture@gmail.com>
2022-02-04 09:50:02 -0500

    correct swith with if else statement

--- 20220204/Problem4/main.cpp
+++ 20220204/Problem4/main.cpp
@@ -11,38 +11,67 @@ int main()
     cout << "Enter an operator (+, -, *, /): ";
     cin >> oper;
 
-    cout << "Enter two numbers: " << endl;
-    cin >> num1 >> num2;
-
-    switch (oper)
+    if (oper == '+' || oper == '-' || oper == '*' || oper == '/')
     {
-    case '+':
-        cout << num1 << " + " << num2 << " = " << num1 + num2;
-        break;
-    case '-':
-        cout << num1 << " - " << num2 << " = " << num1 - num2;
-        break;
-    case '*':
-        cout << num1 << " * " << num2 << " = " << num1 * num2;
-        break;
-    case '/':
-        cout << num1 << " / " << num2 << " = " << num1 / num2;
-        break;
-    default:
-        // operator is doesn't match any case constant (+, -, *, /)
+        cout << "Enter two numbers: " << endl;
+        cin >> num1 >> num2;
+
+        switch (oper)
+        {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+        default:
+            // operator is doesn't match any case constant (+, -, *, /)
+            cout << "Error! The operator is not correct";
+            break;
+        }
+    } else {
         cout << "Error! The operator is not correct";
-        break;
     }
 
     return 0;
 }
 
 /*
-Expected output
+Expected output #1
 
 Enter an operator (+, -, *, /): +
 Enter two numbers: 
 1
 2
 1 + 2 = 3
+
+Expected output #2
+
+Enter an operator (+, -, *, /): -
+Enter two numbers: 
+10
+5
+10 - 5 = 5
+
+Expected output #3
+
+Enter an operator (+, -, *, /): *
+Enter two numbers: 
+5
+5
+5 * 5 = 25
+
+Expected output #4
+
+Enter an operator (+, -, *, /): /
+Enter two numbers: 
+50
+5
+50 / 5 = 10
 */
