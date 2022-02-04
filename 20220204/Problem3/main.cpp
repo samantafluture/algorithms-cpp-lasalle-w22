4178f134f15f4a858768d57f3f2a9df4d1093697
Samanta Fluture <samantafluture@gmail.com>
2022-02-04 08:56:25 -0500

    create program to swap numbers using temporary variable

--- 20220204/Problem3/main.cpp
+++ 20220204/Problem3/main.cpp
@@ -2,24 +2,61 @@
 
 using namespace std;
 
+// EX. 2 - SWAP NUMBERS USING TEMPORARY VARIABLE
+
+int main()
+{
+    int a = 5, b = 10, temp;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << ", temp = " << temp << endl;
+    
+    temp = a; // preserve value a, space in memory
+    a = b; // now a = 10
+    b = temp; // now b = 5
+    
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << ", temp = " << temp << endl;
+    
+    return 0;
+}
+
+/*
+EX. 2 - EXPECTED OUTPUT
+
+Before swapping.
+a = 5, b = 10, temp = 0
+
+After swapping.
+a = 10, b = 5, temp = 5
+*/
+
+/******************************************************************************/
+/* EX. 1 - FIND SIZE OF VARIABLES
+
 int main() {
 
-    // 1- Check size of different types os variables
+    // Check size of different types os variables
     cout << "Size of char: " << sizeof(char) << " byte" << endl;
     cout << "Size of int: " << sizeof(int) << " bytes" << endl;
     cout << "Size of float: " << sizeof(float) << " bytes" << endl;
     cout << "Size of double: " << sizeof(double) << " bytes" << endl;
     cout << "Size of boolean: " << sizeof(bool) << " byte" << endl;
 
-    // 1- Modifications
+    // Modifications
     char A = 'a';
+    bool test = false;
+
     cout << "Size of variable char A: " << sizeof(A) << " byte" << endl;
+    cout << "Size of variable boolean test: " << sizeof(test) << " byte" << endl;
 
     return 0;
 }
 
-/*
-1 - Expected output:
+*/
+
+/* 
+EX 1 - EXPECTED OUTPUT
 
 Size of char: 1 byte
 Size of int: 4 bytes
@@ -28,5 +65,5 @@ Size of double: 8 bytes
 Size of boolean: 1 byte
 
 Size of variable char A: 1 byte
-
+Size of variable boolean test: 1 byte
 */
