29c6ae20255c605cc517259f05bd30b0656a7c5e
Samanta Fluture <samantafluture@gmail.com>
2022-02-24 16:09:22 -0500

    solve do- while loop problems

--- MidtermReview/6-DoWhileLoop/main.cpp
+++ MidtermReview/6-DoWhileLoop/main.cpp
@@ -2,7 +2,50 @@
 
 // Problem 1: Printing Numbers From 1 to 5
 
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        cout << i << " ";
+        i++;
+
+    } while (i < 6);
+
+    return 0;
+}
+*/
 
 // Problem 2: program to find the sum of positive numbers
 // If the user enters a negative number, the loop ends
 // the negative number entered is not added to the sum
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    int sum = 0;
+
+    cout << "Enter a positive number to sum: ";
+    cin >> num;
+
+    do
+    {
+        sum += num;
+
+        cout << "Enter another number: ";
+        cin >> num;
+        
+    } while (num >= 0);
+
+    cout << "The total sum is: " << sum << endl;
+
+    return 0;
+}
