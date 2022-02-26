139a6c4630da94687c1c6b15409769b844adbe19
Samanta Fluture <samantafluture@gmail.com>
2022-02-26 14:48:06 -0500

    new problem using do-while loop

--- PrepMidterm/20220226/main.cpp
+++ PrepMidterm/20220226/main.cpp
@@ -46,12 +46,48 @@ int main() {\
         }
 
         total += number;
-        
+
     }
-    
+
     average = total/5;
 
     cout << "The bigger number you enter is : " << biggest << " and the average is : " << average << endl;
+
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double arr[5], max, avg, total;
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Enter the number " << i + 1 << " : ";
+        cin >> arr[i];
+
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+
+        total += arr[i];
+    }
+
+    avg = total / 5;
+
+    cout << "The bigger number you enter is : " << max << " and the average is : " << avg << endl;
+
+    // print array
+    // for (i = 0; i < 5; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
 
     return 0;
 }
@@ -68,7 +104,7 @@ START
     min := 99999
     WRITE "Enter the first number: "
     READ number
-    REPEAT 
+    REPEAT
         IF (number > max) THEN
             max = number;
         ENDIF
@@ -84,6 +120,7 @@ END
 
 // Question 4
 
+/*
 #include <iostream>
 using namespace std;
 
@@ -106,13 +143,101 @@ int main() {\
         {
             min = number;
         }
-        
+
         cout << "Enter another number or if you want to quit, enter 0 or a negative number: ";
         cin >> number;
 
     } while (number > 0);
-    
+
     cout << "The bigger number you enter is : " << max << " and the smallest number you enter is : " << min << endl;
+
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double arr[10];
+    double max = -999999;
+    double min = 999999;
+    int i;
+
+    cout << "Enter the first number: ";
+    cin >> arr[i];
+
+    do
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+
+        cout << "Enter another number or if you want to quit, enter 0 or a negative number: ";
+        cin >> arr[i];
+
+    } while (arr[i] > 0);
+
+    cout << "The bigger number you enter is : " << max << " and the smallest number you enter is : " << min << endl;
+
+    return 0;
+}
+*/
+
+// Extra
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double number;
+    double max = -99999;
+    double min = 99999;
+    double product = 1;
+    double sum, average;
+    int counter;
+
+    cout << "Enter the first number: ";
+    cin >> number;
+
+    do
+    {
+        if (number > max)
+        {
+            max = number;
+        }
+
+        if (number < min)
+        {
+            min = number;
+        }
+
+        sum += number;
+        product *= number;
+
+        cout << "Enter another number or if you want to quit, enter 0 or a negative number: ";
+        cin >> number;
+
+        counter++;
+
+    } while (number > 0);
+
+    average = sum / counter;
+
+    cout << "Biggest number: " << max << endl;
+    cout << "Smallest number: " << min << endl;
+    cout << "Total sum: " << sum << endl;
+    cout << "Total product: " << product << endl;
+    cout << "Average: " << average << endl;
 
     return 0;
 }
