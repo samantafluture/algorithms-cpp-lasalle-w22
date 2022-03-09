68850e0ea4f942bb81c6a038646187d7811371e8
Samanta Fluture <samantafluture@gmail.com>
2022-03-09 12:02:56 -0500

    new studies about functions

--- 20220309/functions/main.cpp
+++ 20220309/functions/main.cpp
@@ -1,5 +1,112 @@
 // Samanta Gimenez Fluture, 2022-03-09, Studying Functions
 
+// modified code
+
+#include <iostream>
+using namespace std;
+
+double add(double, double);
+double substract(double, double);
+double multiple(double, double);
+double divide(double, double);
+
+int main()
+{
+    double num1, num2;
+    int ans;
+
+    cout << "\n*****************\n";
+    cout << "Simple Calculator\n";
+    cout << "*****************\n\n";
+
+    cout << "Enter the first value: ";
+    cin >> num1;
+
+    cout << "Enter the second value: ";
+    cin >> num2;
+
+    do
+    {
+        cout << "\n* Choose your operation *\n"
+                "Enter 1 for addition (+)\n"
+                "Enter 2 for substraction (-)\n"
+                "Enter 3 for multiplication (*)\n"
+                "Enter 4 for division (/)\n"
+                "Enter 0 to quit\n"
+                "\n>> Your option: ";
+        cin >> ans;
+
+        switch (ans)
+        {
+        case 1:
+        {
+            cout << "\n>> Your result: "
+                 << num1 << " + " << num2 << " = " << add(num1, num2) << endl;
+            break;
+        }
+        case 2:
+        {
+            cout << "\n>> Your result: "
+                 << num1 << " - " << num2 << " = " << substract(num1, num2) << endl;
+            break;
+        }
+        case 3:
+        {
+            cout << "\n>> Your result: "
+                 << num1 << " * " << num2 << " = " << multiple(num1, num2) << endl;
+            break;
+        }
+        case 4:
+        {
+            if (num2 != 0)
+            {
+                cout << "\n>> Your result: "
+                     << num1 << " / " << num2 << " = " << divide(num1, num2) << endl;
+            }
+            else
+            {
+                cout << "\nError, can't divide by 0!" << endl;
+            }
+            break;
+        }
+        case 0:
+        {
+            cout << "\nGood-bye!" << endl;
+            break;
+        }
+        default:
+        {
+            cout << "\nError, invalid input!" << endl;
+            break;
+        }
+        }
+
+    } while (ans != 0);
+
+    return 0;
+}
+
+double add(double a, double b)
+{
+    return (a + b);
+}
+
+double substract(double a, double b)
+{
+    return (a - b);
+}
+
+double multiple(double a, double b)
+{
+    return (a * b);
+}
+
+double divide(double a, double b)
+{
+    return (a / b);
+}
+
+/*
 #include <iostream>
 using namespace std;
 
@@ -14,18 +121,24 @@ double divide(double, double);
 
 int main()
 {
-    int num1, num2;
+    double num1, num2;
 
-    cout << "Enter the first value: " << endl;
+    cout << "Enter the first value: ";
     cin >> num1;
 
-    cout << "Enter the second value: " << endl;
+    cout << "Enter the second value: ";
     cin >> num2;
 
     cout << num1 << " + " << num2 << " = " << add(num1, num2) << endl;
     cout << num1 << " - " << num2 << " = " << substract(num1, num2) << endl;
     cout << num1 << " * " << num2 << " = " << multiple(num1, num2) << endl;
-    cout << num1 << " / " << num2 << " = " << divide(num1, num2) << endl;
+
+    if (num2 != 0)
+    {
+        cout << num1 << " / " << num2 << " = " << divide(num1, num2) << endl;
+    } else {
+        cout << "Error, can't divide by 0!" << endl;
+    }
 
     return 0;
 }
@@ -50,12 +163,11 @@ double multiple(double a, double b)
 double divide(double a, double b)
 {
     return (a / b);
-} 
-
+}
+*/
 
 // ***************************************************
 // function overloading
-
 
 /*
 #include <iostream>
@@ -90,10 +202,8 @@ int main()
 }
 */
 
-
 // ***************************************************
 // c++ library functions
-
 
 /*
 #include <iostream>
