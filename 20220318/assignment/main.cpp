5cb908425ed25a7427c7847f647c9eb340b4d971
Samanta Fluture <samantafluture@gmail.com>
2022-03-18 13:48:14 -0400

    create template of assign about functions

---
+++ 20220318/assignment/main.cpp
@@ -0,0 +1,19 @@
+// Samanta Gimenez Fluture, 2022-03-18, Working with functions and arrays
+
+// function declaration
+double multiply(int, double);
+void print_value(double);
+
+int main() {
+    
+}
+
+// function definition
+double multiply(int a, double b) {
+    return (a * b);
+}
+
+void print_value(double c) {
+    cout << c << endl;
+}
+
